32b49654c2009948ea96d01c17740839dcb26308
kevinchin37 <kevin.chin34@gmail.com>
2019-08-19 21:47:45 -0400

    Add links for creating and editing posts along with a button for deleting

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,15 @@
+<a href="/admin/posts/create/">Create New Post</a>
+
 <ul>
 @foreach ($posts as $post)
-    <li><a href="/admin/posts/{{ $post->id }}">{{ $post->title }}</a></li>
+    <li>
+        <a href="/admin/posts/{{ $post->id }}">{{ $post->title }}</a> -
+        <a href="/admin/posts/{{ $post->id }}/edit/">Edit</a>
+        <form action="/admin/posts/{{ $post->id }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Delete</button>
+        </form>
+    </li>
 @endforeach
 </ul>
