a525b40eea519a07a1f74577def9300e6ed2081a
kevinchin37 <kevin.chin34@gmail.com>
2019-08-25 16:38:24 -0400

    Add dropdown for selecting a category on post creation page

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -2,6 +2,14 @@
     @csrf
     <input type="text" name="title" placeholder="Post title"/><br/><br/>
     <textarea id="" cols="30" rows="10" name="body"></textarea><br/><br/>
+
+    <select name="category">
+        <option disabled selected value> -- Select a Category -- </option>
+        @foreach ($categories as $category)
+            <option value="{{ $category->id }}">{{ $category->name }}</option>
+        @endforeach
+    </select>
+
     <button type="submit">Publish</button>
 </form>
 
