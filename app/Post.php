fc3ad33ec50ebd13ebe8dc31f714ba9e969d80d1
kevinchin37 <kevin.chin34@gmail.com>
2019-08-25 16:35:06 -0400

    Add many to many relationship with category:

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Category;
 
 class Post extends Model
 {
     protected $guarded = [];
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
 }
