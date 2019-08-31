cb54abf93f089540732ba15f4fdc1e6b2398afc1
kevinchin37 <kevin.chin34@gmail.com>
2019-08-30 21:49:33 -0400

    Change to move post templates into it's own folder

--- resources/views/admin/edit.blade.php
+++
@@ -1,7 +0,0 @@
-<form action="/admin/posts/{{ $post->id }}" method="POST">
-    @csrf
-    @method('PATCH')
-    <input type="text" name="title" value="{{ $post->title }}"/>
-    <textarea id="" cols="30" rows="10" name="body">{{ $post->body }}</textarea>
-    <button type="submit">Update</button>
-</form>
