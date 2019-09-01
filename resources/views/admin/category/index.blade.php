dc2eb474a4b9d7eaa204ca7e7fd38c2bd14527a1
kevinchin37 <kevin.chin34@gmail.com>
2019-09-01 17:23:20 -0400

    Update blade files to extend the master layout

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,7 +1,38 @@
-<a href="/admin/categories/create">Add a Category</a>
+@extends('admin.layouts.master')
 
-<ul>
-    @foreach ($categories as $category)
-        <li><a href="/admin/categories/{{ $category->id }}">{{ $category->name }}</a></li>
-    @endforeach
-</ul>
+@section('header_title', 'Categories')
+
+@section('header_links')
+    <form action="/admin/categories" method="POST" class="form-inline">
+        @csrf
+        <div class="form-group">
+            <label class="sr-only" for="new-category">Category</label>
+            <input type="text" name="name" id="new-category" class="mr-sm-2">
+            <button type="submit" class="btn btn-primary">Add Category</button>
+        </div>
+    </form>
+@endsection
+
+@section('main_content')
+    <div class="row">
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Category ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Post Count</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($categories as $category)
+                    <tr>
+                        <th scope="row">{{ $category->id }}</th>
+                        <td><a href="/admin/categories/{{ $category->id }}">{{ $category->name }}</a></td>
+                        <td><a href="/admin/categories/{{ $category->id }}">{{ $category->posts->count() }}</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
--- resources/views/admin/category/show.blade.php
+++ resources/views/admin/category/show.blade.php
@@ -1,8 +1,34 @@
+@extends('admin.layouts.master')
 
-@if ($category->posts->count())
-    <ul>
-        @foreach ($category->posts as $post)
-            <li><a href="/admin/posts/{{ $post->id }}/edit">{{ $post->title }}</a></li>
-        @endforeach
-    </ul>
-@endif
+@section('header_title', $category->name)
+
+@section('main_content')
+    <div class="row">
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Post ID</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($category->posts as $post)
+                    <tr>
+                        <th scope="row">{{ $post->id }}</th>
+                        <td><a href="/admin/posts/{{ $post->id }}/edit">{{ $post->title }}</a></td>
+                        <td>
+                            <form action="/admin/posts/{{ $post->id }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
+
