5cfa0c8bad1d7e293de3fc4371f541c90f8f4464
kevinchin37 <kevin.chin34@gmail.com>
2019-09-02 15:21:37 -0400

    Add category routes

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,15 @@ Route::get('/', function () {
 Route::get('admin', 'AdminPostController@index');
 Route::get('admin/posts', 'AdminPostController@postIndex');
 Route::get('admin/posts/create', 'AdminPostController@create');
-// Route::get('admin/posts{post}', 'AdminPostController@show');
 Route::get('admin/posts/{post}/edit', 'AdminPostController@edit');
 Route::post('admin/posts', 'AdminPostController@store');
 Route::patch('admin/posts/{post}', 'AdminPostController@update');
 Route::delete('admin/posts/{post}', 'AdminPostController@destroy');
+
+
+Route::get('admin/categories', 'CategoryController@index');
+Route::get('admin/categories/{category}', 'CategoryController@show');
+Route::post('admin/categories/', 'CategoryController@store');
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
