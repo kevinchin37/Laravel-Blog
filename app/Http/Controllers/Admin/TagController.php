4c35dd9af6e4b4904dab2165c93b88007a7df54a
kevinchin37 <kevin.chin34@gmail.com>
2019-11-23 17:18:36 -0500

    Add tag controller

---
+++ app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Tag;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.tag.index', [
+            'tags' => Tag::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+        $attributes = $this->validateRequest();
+        $tag = new tag;
+        $attributes['slug'] = str_slug($attributes['name']);
+        $tag->create($attributes);
+
+        return redirect('/admin/tags');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tag $tag)
+    {
+        return view('admin.tag.show', ['tag' => $tag]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tag $tag)
+    {
+        return view('admin.tag.edit', [
+            'tag' => $tag
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Tag $tag)
+    {
+        $attributes = $this->validateRequest();
+        $tag->update($attributes);
+
+        return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+        return back();
+    }
+
+    public function validateRequest() {
+        return request()->validate([
+            'name' => 'required|unique:categories',
+        ],[
+            'name.required' => 'The tag name is required.',
+            'name.unique' => 'The tag name already exists.',
+        ]);
+    }
+}
