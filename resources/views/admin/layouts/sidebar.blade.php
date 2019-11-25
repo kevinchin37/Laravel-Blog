68bbf238bc9a299f5fd6edb9402249e8884938db
kevinchin37 <kevin.chin34@gmail.com>
2019-11-24 21:04:17 -0500

    Add link to roles page

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -3,5 +3,6 @@
     <a class="list-group-item" href="/admin/categories">Categories</a>
     <a class="list-group-item" href="/admin/tags">Tags</a>
     <a class="list-group-item" href="/admin/users">Users</a>
+    <a class="list-group-item" href="/admin/roles">Roles</a>
     @yield('sidebar_items')
 </div>
