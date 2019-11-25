3992a1c6c41f39d41b761b6bac3cf81d30274ffc
kevinchin37 <kevin.chin34@gmail.com>
2019-11-24 21:05:36 -0500

    Add user blade templates

---
+++ resources/views/admin/user/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('admin.layouts.master')
+
+@section('header_title', 'Edit')
+
+@section('main_content')
+    <div class="row">
+        <div class="col-md-12">
+            <form action="/admin/users/{{ $user->id }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PATCH')
+                <div class="form-group">
+                    <input type="text" class="form-control" name="name" placeholder="Username" value="{{ $user->name }}"/>
+                </div>
+
+                <div class="form-group">
+                    <h4>Role</h4>
+                    <select>
+                        @foreach ($roles as $role)
+                            <option value="{{ $role->name }}" {{ ($user->role == $role) ? 'selected' : '' }}>{{ $role->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <button type="submit" class="btn btn-outline-dark mt-3">Update</button>
+            </form>
+        </div>
+    </div>
+@endsection
