145d9235ed9744d15466b33daa7fcdf74734b116
kevinchin37 <kevin.chin34@gmail.com>
2019-11-24 19:57:53 -0500

    Add method for checking for user's role

--- app/User.php
+++ app/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     public function role() {
         return $this->belongsTo(Role::class);
     }
+
+    public function hasRole($role) {
+        return $this->role->name === $role;
+    }
 }
