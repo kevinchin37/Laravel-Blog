6807e3f295253497b457c0f2c80c7276910a8f56
kevinchin37 <kevin.chin34@gmail.com>
2019-12-01 16:05:15 -0500

    Add Post policies

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Post;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any posts.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user) {
+        return $user->hasRole('Editor');
+    }
+
+    /**
+     * Determine whether the user can view the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function view(User $user, Post $post) {
+        return $user->hasRole('Editor');
+    }
+
+    /**
+     * Determine whether the user can create posts.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user) {
+        return $user->hasRole('Editor');
+    }
+
+    /**
+     * Determine whether the user can update the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function update(User $user, Post $post) {
+        return $user->hasRole('Editor');
+    }
+
+    public function before($user, $ability) {
+        if ($user->hasRole('Admin')) {
+            return true;
+        }
+    }
+}
