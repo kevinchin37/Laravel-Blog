5d6cfd96a12f70f5c8508e1e0a47a15a78467da5
kevinchin37 <kevin.chin34@gmail.com>
2019-12-01 16:45:53 -0500

    Add CategoryPolicy

---
+++ app/Policies/CategoryPolicy.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Category;
+use App\Policies\PolicyTrait;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CategoryPolicy
+{
+    use HandlesAuthorization;
+    use PolicyTrait;
+
+    /**
+     * Determine whether the user can create categories.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user) {
+        return $user->hasRole('Editor');
+    }
+
+    /**
+     * Determine whether the user can update the category.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Category  $category
+     * @return mixed
+     */
+    public function update(User $user, Category $category) {
+        return $user->hasRole('Editor');
+    }
+
+    /**
+     * Determine whether the user can delete the category.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Category  $category
+     * @return mixed
+     */
+    public function delete(User $user, Category $category) {
+        return $user->hasRole('Admin');
+    }
+}
