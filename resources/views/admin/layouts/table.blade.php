a3d8aad1572f5b611d5c013a11225e6d2a481035
kevinchin37 <kevin.chin@cbs.com>
2019-12-05 16:05:14 -0500

    Add resuable blade template for building tables

---
+++ resources/views/admin/layouts/table.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.layouts.master')
+
+@section('main_content')
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        @yield('table_header_column')
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @yield('table_body')
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
