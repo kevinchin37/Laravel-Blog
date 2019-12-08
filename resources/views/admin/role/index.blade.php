ac9d42bcf3901dc097fa9e1d3f96735905ed7d3f
kevinchin37 <kevin.chin34@gmail.com>
2019-12-07 20:44:34 -0500

    Update role template to extend table.blade

--- resources/views/admin/role/index.blade.php
+++ resources/views/admin/role/index.blade.php
@@ -1,42 +1,26 @@
-@extends('admin.layouts.master')
+@extends('admin.layouts.table')
 
 @section('header_title', 'Roles')
 
-@section('main_content')
-    <div class="row">
-        <div class="col-md-12">
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
+@section('table_header_columns')
+    <th scope="col">Name</th>
+    <th scope="col">Action</th>
+@endsection
 
-                <tbody>
-                    @foreach ($roles as $role)
-                        <tr>
-                            <td>{{ $role->name }}</td>
-                            <td>
-                                <form action="/admin/roles/{{ $role->id }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-        </div>
-    </div>
+@section('table_body')
+    <tbody>
+        @foreach ($roles as $role)
+            <tr>
+                <td>{{ $role->name }}</td>
+
+                <td>
+                    <form action="/admin/roles/{{ $role->id }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
 @endsection
