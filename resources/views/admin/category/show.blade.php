32556ab2aef5fd863fb404e9b07334e635a1fe99
kevinchin37 <kevin.chin34@gmail.com>
2019-12-07 21:19:27 -0500

    Update category detail template to extend table.blade

--- resources/views/admin/category/show.blade.php
+++ resources/views/admin/category/show.blade.php
@@ -1,36 +1,25 @@
-@extends('admin.layouts.master')
+@extends('admin.layouts.table')
 
 @section('header_title', $category->name)
 
-@section('main_content')
-    <div class="row">
-        <div class="col-md-12">
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">Post ID</th>
-                        <th scope="col">Title</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    @foreach ($category->posts as $post)
-                        <tr>
-                            <th scope="row">{{ $post->id }}</th>
-                            <td><a href="/admin/posts/{{ $post->slug }}/edit">{{ $post->title }}</a></td>
-                            <td>
-                                <form action="/admin/posts/{{ $post->slug }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
+@section('table_header_columns')
+    <th scope="col">Post ID</th>
+    <th scope="col">Title</th>
+    <th scope="col">Action</th>
 @endsection
 
+@section('table_body')
+    @foreach ($category->posts as $post)
+    <tr>
+        <th scope="row">{{ $post->id }}</th>
+        <td><a href="/admin/posts/{{ $post->slug }}/edit">{{ $post->title }}</a></td>
+        <td>
+            <form action="/admin/posts/{{ $post->slug }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit">Delete</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+@endsection
