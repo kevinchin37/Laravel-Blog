a6548ddceb15fb5193fde46eec6730d4d6ed5904
kevinchin37 <kevin.chin34@gmail.com>
2019-12-09 20:52:57 -0500

    Remove error snippet and extra div

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -48,17 +48,8 @@
                     </div>
                 </div>
 
-                <div class=""><button type="submit" class="btn btn-primary">Create Post</button></div>
+                <button type="submit" class="btn btn-primary">Create Post</button>
             </form>
         </div>
     </div>
-
-    @if ($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
-
 @endsection
