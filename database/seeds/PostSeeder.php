0c26ad68880a59c9c37ea2f3fb0f3d90ed759ea7
kevinchin37 <kevin.chin@cbs.com>
2019-12-16 16:29:01 -0500

    Update post seeder to create a tag per post

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -13,9 +13,11 @@ class PostSeeder extends Seeder
     public function run() {
         DB::table('posts')->delete();
         DB::table('categories')->delete();
+        DB::table('tags')->delete();
 
         factory(App\Post::class, 50)->create()->each(function($post) {
             $post->categories()->save(factory(App\Category::class)->make());
+            $post->tags()->save(factory(App\Tag::class)->make());
         });
     }
 }
