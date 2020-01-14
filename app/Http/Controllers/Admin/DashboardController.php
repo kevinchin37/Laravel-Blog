fa39abfee7e82c9967f67241d71eff9234264b38
kevinchin37 <kevin.chin34@gmail.com>
2020-01-13 20:22:06 -0500

    Add admin DashboardController

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Post;
+use App\Activity;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Activity $activity) {
+        return view('admin.dashboard.index', [
+            'posts' => Post::latest()->get()->take(5),
+            'activities' => $activity->latest()->get()->take(5),
+        ]);
+    }
+}
