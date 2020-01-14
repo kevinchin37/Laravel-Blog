c912ba0056b948bdab2d0ed5350fbc556cd11677
kevinchin37 <kevin.chin34@gmail.com>
2020-01-13 20:25:29 -0500

    Remove unused http/request include

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Post;
 use App\Activity;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
