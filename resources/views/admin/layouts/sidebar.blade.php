adccd91a894f4c52f8295032973e5860af99d9b6
kevinchin37 <kevin.chin34@gmail.com>
2020-01-13 20:12:35 -0500

    Add dashboard link to sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,4 +1,5 @@
 <div class="list-group list-group-flush">
+    <a class="list-group-item" href="/admin">Dashboard</a>
     <a class="list-group-item" href="/admin/posts">Posts</a>
     <a class="list-group-item" href="/admin/categories">Categories</a>
     <a class="list-group-item" href="/admin/tags">Tags</a>
