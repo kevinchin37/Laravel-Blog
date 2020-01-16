8fe0502941a7cbada094a99df384a8800c8281fc
kevinchin37 <kevin.chin@cbs.com>
2020-01-16 16:06:19 -0500

    Upate file path

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -6,7 +6,7 @@ use App\Post;
 use App\Category;
 use App\Tag;
 use App\Http\Controllers\Controller;
-use App\Support\Services\ImageService;
+use App\Http\Support\Services\ImageService;
 
 class PostController extends Controller {
     /**
--- app/Http/Support/Services/ImageService.php
+++ app/Http/Support/Services/ImageService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Support\Services;
+namespace App\Http\Support\Services;
 
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
