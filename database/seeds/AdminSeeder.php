ddf4c0591e739d7591ca183d0498cb112e348e89
kevinchin37 <kevin.chin34@gmail.com>
2020-01-18 20:59:49 -0500

    Add seeder to create an initial admin

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $now = Carbon::now()->format('Y-m-d H:i:s');
+
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'role_id' => 1, // admin,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+    }
+}
