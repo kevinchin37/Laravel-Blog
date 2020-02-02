3021f589a5f4a7f2bac61c04a681e3e8ac8b8a02
kevinchin37 <kevin.chin34@gmail.com>
2020-02-01 21:50:28 -0500

    Add a view composer for getting all categories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,13 @@
 
 namespace App\Providers;
 
+use App\Tag;
 use App\Post;
 use App\Category;
-use App\Tag;
+use App\Observers\TagObserver;
 use App\Observers\PostObserver;
 use App\Observers\CategoryObserver;
-use App\Observers\TagObserver;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -34,5 +35,9 @@ class AppServiceProvider extends ServiceProvider
         Post::observe(PostObserver::class);
         Category::observe(CategoryObserver::class);
         Tag::observe(TagObserver::class);
+
+        View::composer(['admin.post.create', 'admin.layouts.search'], function($view) {
+            $view->with('categories', Category::all());
+        });
     }
 }
