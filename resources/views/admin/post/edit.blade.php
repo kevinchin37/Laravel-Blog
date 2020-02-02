43186696e76ef092c729f7bb52bdbbd7c670411a
kevinchin37 <kevin.chin34@gmail.com>
2020-02-01 22:31:20 -0500

    Change to bold meta keys and add post link

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -29,11 +29,12 @@
 
                     <div class="col-md-6">
                         <ul>
-                            <li>ID: {{ $post->id }}</li>
-                            <li>Slug: {{ $post->slug }}</li>
-                            <li>Author: {{ $post->author->name }}</li>
-                            <li>Published Date: {{ $post->created_at }}</li>
-                            <li>Last Modified: {{ $post->updated_at }}</li>
+                            <li><strong>ID:</strong> {{ $post->id }}</li>
+                            <li><strong>Slug:</strong> {{ $post->slug }}</li>
+                            <li><strong>Link:</strong> <a href="/post/{{ $post->slug }}">{{ url('post/' . $post->slug) }}</a></li>
+                            <li><strong>Author:</strong> {{ $post->author->name }}</li>
+                            <li><strong>Published Date:</strong> {{ $post->created_at }}</li>
+                            <li><strong>Last Modified:</strong> {{ $post->updated_at }}</li>
                         </ul>
                     </div>
                 </div>
