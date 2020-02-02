6cf79bb7bc0ba489f9949b1648445cbfece29b63
kevinchin37 <kevin.chin34@gmail.com>
2020-02-01 21:55:29 -0500

    Add new search blade template

---
+++ resources/views/admin/layouts/search.blade.php
@@ -0,0 +1,33 @@
+<div class="border border-secondary p-3 mb-3">
+    <form action="/admin/search" type="POST">
+        @csrf
+        <div class="form-group">
+            <label for="search-title">Search Post</label>
+            <input type="text" class="form-control" id="search-title" name="title" placeholder="Post Title"/>
+        </div>
+
+        <div class="form-group">
+            <div class="row">
+                <div class="col-md-4">
+                    <label for="search-author">Author</label>
+                    <input type="text" class="form-control" id="search-author" name="author" placeholder="Author Name" value="{{ old('author') }}"/>
+                </div>
+                <div class="col-md-4">
+                    <label for="search-category">Category</label>
+                    <select class="custom-select" id="search-category" name="category">
+                        <option value="none" selected disabled hidden>Select a Category</option>
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-md-4">
+                    <label for="search-tag">Tag</label>
+                    <input type="text" class="form-control" id="search-tag" name="taxonomy[tag]" placeholder="Tag Name"/>
+                </div>
+            </div>
+        </div>
+        <button class="btn btn-primary" type="submit">Submit</button>
+    </form>
+</div>
+
