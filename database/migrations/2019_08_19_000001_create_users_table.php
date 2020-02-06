b62257dfdc3922f558d5f4c5e541781452576dfd
kevinchin37 <kevin.chin34@gmail.com>
2020-02-06 18:41:56 -0500

    Remove nullable from role_id

--- database/migrations/2019_08_19_000001_create_users_table.php
+++ database/migrations/2019_08_19_000001_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->unsignedBigInteger('role_id')->nullable();
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade')->nullable();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->timestamps();
         });
     }
