3bc88b4e0d4cc96451c3d2b641fb4bf33ad5bc47
kevinchin37 <kevin.chin34@gmail.com>
2020-02-08 21:29:09 -0500

    Add Invitation table migration

---
+++ database/migrations/2020_02_04_013026_create_invitations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvitationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('role_id');
+            $table->string('email')->unique();
+            $table->string('token')->unique();
+            $table->string('status')->default('pending');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invitations');
+    }
+}
