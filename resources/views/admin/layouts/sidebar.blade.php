9d0fa502ef19c5010c17bd8d46b898a8443d6373
kevinchin37 <kevin.chin34@gmail.com>
2020-02-08 21:31:01 -0500

    Add invitations to sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,9 +1,11 @@
+{{-- @TODO add submenus later --}}
 <div class="list-group list-group-flush">
     <a class="list-group-item" href="/admin">Dashboard</a>
     <a class="list-group-item" href="/admin/posts">Posts</a>
     <a class="list-group-item" href="/admin/categories">Categories</a>
     <a class="list-group-item" href="/admin/tags">Tags</a>
     <a class="list-group-item" href="/admin/users">Users</a>
+    <a class="list-group-item" href="/admin/invitations">Invitations</a>
     <a class="list-group-item" href="/admin/roles">Roles</a>
     @yield('sidebar_items')
 </div>
