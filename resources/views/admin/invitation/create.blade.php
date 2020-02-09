589e701d0cdb52edf2f984e393c7922d267e8fd8
kevinchin37 <kevin.chin34@gmail.com>
2020-02-08 21:29:28 -0500

    Add Invitation templates

---
+++ resources/views/admin/invitation/create.blade.php
@@ -0,0 +1,28 @@
+@extends('admin.layouts.master')
+
+@section('header_title', 'Invite')
+
+@section('main_content')
+    <div class="row">
+        <div class="col-md-12">
+            <form action="/admin/invitations" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="user-email">E-mail</label>
+                    <input type="text" id="user-email" class="form-control w-25" name="email" placeholder="person@email.com"/>
+                </div>
+
+                <div class="form-group">
+                    <label for="user-role">Role</label>
+                    <select id="user-role" class="form-control w-25" name="role_id">
+                        @foreach ($roles as $role)
+                            <option value="{{ $role->id }}">{{ $role->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <button type="submit" class="btn btn-primary mt-3">Create</button>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/invitation/index.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layouts.table')
+
+@section('header_title', 'Invitations')
+
+@section('header_links')
+    <a class="btn btn-primary" href="/admin/invitations/create">Create Invite Link</a>
+@endsection
+
+@section('table_header_columns')
+    @if ($invitations->isEmpty())
+        @component('admin.components.alerts.empty')
+            @slot('message')
+                No current invitations.
+            @endslot
+        @endcomponent
+    @else
+        <th scope="col">ID</th>
+        <th scope="col">Email</th>
+        <th scope="col">Link</th>
+        <th scope="col">Status</th>
+    @endif
+@endsection
+
+@section('table_body')
+    @foreach ($invitations as $invitation)
+    <tr>
+        <th scope="row">{{ $invitation->id }}</th>
+        <td>{{ $invitation->email }}</td>
+        {{-- <td><a href="{{ url('invitation/?invitation_token=' . $invitation->token) }}">Invite Link</a></td> --}}
+        <td><a href="{{ url('invitation/' . $invitation->token) }}">Invite Link</a></td>
+        {{-- <td><a href="{{ url('invitation/' . $invitation->id . '?invitation_token=' . $invitation->token) }}">Invite Link</a></td> --}}
+        <td>{{ $invitation->status }}</td>
+    </tr>
+    @endforeach
+@endsection
+
+@section('pagination_links')
+    {{ $invitations->links() }}
+@endsection
