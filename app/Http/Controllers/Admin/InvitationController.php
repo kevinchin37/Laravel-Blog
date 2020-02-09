c7b49aea08a77202ee78158a0e51af7ddfd2eae3
kevinchin37 <kevin.chin34@gmail.com>
2020-02-08 21:28:45 -0500

    Add Invitation controller

---
+++ app/Http/Controllers/Admin/InvitationController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Role;
+use App\Invitation;
+use App\Http\Controllers\Controller;
+
+class InvitationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        return view('admin.invitation.index', [
+            'invitations' => Invitation::paginate(15)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() {
+        return view('admin.invitation.create', [
+            'roles' => Role::all()
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store() {
+        $attributes = request()->validate([
+            'email' => 'required',
+            'role_id' => 'required',
+        ]);
+
+        $invitation = new Invitation();
+        $attributes['token'] = $invitation->generateToken($attributes['email']);
+        $invitation->create($attributes);
+
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Invitation  $invitation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Invitation $invitation) {
+        return view('auth.register', [
+            'invitation' => $invitation
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Invitation  $invitation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Invitation $invitation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Invitation  $invitation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Invitation $invitation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Invitation  $invitation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Invitation $invitation)
+    {
+        //
+    }
+
+    public function validateRequest() {
+        return request()->validate([
+            'email' => 'required',
+            'role' => 'required',
+        ]);
+    }
+}
