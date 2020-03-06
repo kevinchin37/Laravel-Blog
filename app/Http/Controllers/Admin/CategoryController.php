c19d81d2251b22485db98f0a6d4420485fa03ee2
kevinchin37 <kevin.chin@cbs.com>
2020-03-06 16:02:57 -0500

    Update order of posts by category (desc)

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -55,7 +55,9 @@ class CategoryController extends Controller
     public function show(Category $category) {
         return view('admin.category.show', [
             'category' => $category,
-            'categoryPosts' => $category->posts()->paginate(15),
+            'categoryPosts' => $category->posts()
+                ->orderBy('created_at', 'desc')
+                ->paginate(15),
         ]);
     }
 
