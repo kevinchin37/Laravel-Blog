e3b3055036f837ee2baedbc8fb39573ce4e9a237
kevinchin37 <kevin.chin@cbs.com>
2020-03-06 16:15:43 -0500

    Remove unused class imports

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Post;
-use App\Category;
 use App\Tag;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Role;
 use App\User;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class UserController extends Controller
