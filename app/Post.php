5da520cf956002773ecf3bdb0ba2aa3cf2df08b1
kevinchin37 <kevin.chin34@gmail.com>
2020-03-09 21:40:27 -0400

    Add documentation to Post methods

--- app/Post.php
+++ app/Post.php
@@ -16,10 +16,21 @@ class Post extends Model
     use LoggableActivity;
     use Sluggable;
 
+    /**
+     * Post's categories
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function categories() {
         return $this->belongsToMany(Category::class)->using(CategoryPost::class);
     }
 
+    /**
+     * Attach categories to a post
+     *
+     * @param array $attributes
+     * @return void
+     */
     public function addCategories($attributes) {
         if (empty($attributes['categories'])) return;
 
@@ -28,15 +39,32 @@ class Post extends Model
         }
     }
 
+    /**
+     * Update categories of a post
+     *
+     * @param array $attributes
+     * @return void
+     */
     public function updateCategories($attributes) {
         $categories = !empty($attributes['categories']) ? $attributes['categories'] : null;
         $this->categories()->sync($categories);
     }
 
+    /**
+     * Post's tags
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function tags() {
         return $this->belongsToMany(Tag::class)->using(PostTag::class);;
     }
 
+    /**
+     * Attach tag(s) to a post
+     *
+     * @param array $attributes
+     * @return void
+     */
     public function addTags($attributes) {
         if (empty($attributes['tags'])) return;
 
@@ -45,11 +73,22 @@ class Post extends Model
         }
     }
 
+    /**
+     * Update tag(s) of a post
+     *
+     * @param array $attributes
+     * @return void
+     */
     public function updateTags($attributes) {
         $tags = !empty($attributes['tags']) ? $attributes['tags'] : null;
         $this->tags()->sync($tags);
     }
 
+    /**
+     * Post's author
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function author() {
         return $this->belongsTo(User::class, 'user_id');
     }
@@ -63,6 +102,12 @@ class Post extends Model
         return 'slug';
     }
 
+    /**
+     * Format a post's date
+     *
+     * @param string $date
+     * @return string
+     */
     public function getCreatedAtAttribute($date) {
         return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('m/d/y, g:i:s A');
     }
