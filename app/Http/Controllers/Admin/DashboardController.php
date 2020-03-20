1c3316981f67c4fa680e60b648882d519f2e8dc8
kevinchin37 <kevin.chin34@gmail.com>
2020-03-19 21:57:15 -0400

    Update method of rendering widgets on dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Tag;
-use App\Post;
-use App\Activity;
-use App\Category;
-use App\Widgets\Widget;
-use App\Widgets\ActivityLog;
-use App\Widgets\LatestEntries;
+use App\Widgets\WidgetBuilder;
+use App\Widgets\Latest\LatestPosts;
 use App\Http\Controllers\Controller;
+use App\Widgets\Latest\LatestCategories;
 
 class DashboardController extends Controller
 {
@@ -21,10 +17,8 @@ class DashboardController extends Controller
     public function index() {
         return view('admin.dashboard.index', [
             'widgets' => [
-                (new Widget(new LatestEntries(new Post, 5), 'Latest Posts'))->getSettings(),
-                (new Widget(new LatestEntries(new Category, 5), 'Latest Categories'))->getSettings(),
-                (new Widget(new LatestEntries(new Tag, 5), 'Latest Tags', 4))->getSettings(),
-                (new Widget(new ActivityLog(new Activity, 3), 'Activities', 8))->getSettings(),
+                (new WidgetBuilder(new LatestPosts, 'Latest Posts'))->getSettings(),
+                (new WidgetBuilder(new LatestCategories, 'Latest Categories'))->getSettings(),
             ]
         ]);
     }
