cf5767a06ceb41166d9623be36eff4224327fe48
kevinchin37 <kevin.chin@cbs.com>
2020-04-24 16:50:21 -0400

    Add many-to-many relation with Permission

--- app/Role.php
+++ app/Role.php
@@ -3,12 +3,17 @@
 namespace App;
 
 use App\User;
+use App\Permission;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
     public function users() {
         return $this->hasMany(User::class);
+    }
+
+    public function permissions() {
+        return $this->belongsToMany(Permission::class);
     }
 
     /**
