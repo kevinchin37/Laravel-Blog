ae6e3c72fff01d5fffef0eed352e8e69f57510f0
kevinchin37 <kevin.chin@cbs.com>
2020-04-24 15:38:40 -0400

    Change route key to slug

--- app/Role.php
+++ app/Role.php
@@ -10,4 +10,13 @@ class Role extends Model
     public function users() {
         return $this->hasMany(User::class);
     }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName() {
+        return 'slug';
+    }
 }
