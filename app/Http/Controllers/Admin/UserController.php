a49d3a471045e4fba501a011fd6ffc61d53a1813
kevinchin37 <kevin.chin@cbs.com>
2020-05-03 15:42:51 -0400

    Change to get all users except owner

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
      */
     public function index() {
         return view('admin.user.index', [
-            'users' => User::with('role')->get(),
+            'users' => User::whereNotIn('role_id', [Role::OWNER_ROLE_ID])->with('role')->get(),
         ]);
     }
 
