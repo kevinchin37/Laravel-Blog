fa1b37a6708241550c77b5cb5d94ae4484064364
kevinchin37 <kevin.chin@cbs.com>
2020-05-09 15:25:45 -0400

    Remove owner role from dropdown

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -33,7 +33,7 @@ class UserController extends Controller
     public function edit(User $user) {
         return view('admin.user.edit', [
             'user' => $user,
-            'roles' => Role::all(),
+            'roles' => Role::whereNotIn('id', [Role::OWNER_ROLE_ID])->get(),
         ]);
     }
 
