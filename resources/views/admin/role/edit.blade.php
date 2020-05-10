7100c7c370214c41e0943ba2b13eb131c89becc6
kevinchin37 <kevin.chin@cbs.com>
2020-05-10 12:52:47 -0400

    Remove extra space and tabs

--- resources/views/admin/role/edit.blade.php
+++ resources/views/admin/role/edit.blade.php
@@ -6,33 +6,33 @@
 	<div class="row post-edit-wrapper">
 		<div class="col-md-12">
 			<form action="/admin/roles/{{ $role->slug }}" method="POST">
-				@csrf
-				@method('PATCH')
+                @csrf
+                @method('PATCH')
 
-			   <div class="form-group">
-					<label for="role-name">Name</label>
-					<input id="role-name" class="form-control w-25" type="text" name="name" value="{{ $role->name }}" />
-			   </div>
+                <div class="form-group">
+                    <label for="role-name">Name</label>
+                    <input id="role-name" class="form-control w-25" type="text" name="name" value="{{ $role->name }}" />
+                </div>
 
-				<label>Permissions</label>
-				<div class="form-group">
-					@foreach ($permissions as $permission)
-						<div class="form-check form-check-inline">
-							<div class="form-check-input">
-							<input type="checkbox" name="permissions[]" value="{{ $permission->id }}" id="{{ $permission->action }}"
-								{{ $role->permissions->contains('action', $permission->action) ? 'checked' : '' }} />
+                <label>Permissions</label>
+                <div class="form-group">
+                    @foreach ($permissions as $permission)
+                        <div class="form-check form-check-inline">
+                            <div class="form-check-input">
+                                <input type="checkbox" name="permissions[]" value="{{ $permission->id }}" id="{{ $permission->action }}"
+                                    {{ $role->permissions->contains('action', $permission->action) ? 'checked' : '' }} />
 
-								<label class="form-check-label" for="{{ $permission->action }}">
-									{{ $permission->action }}
-								</label>
-							</div>
-						</div>
-					@endforeach
-				</div>
+                                <label class="form-check-label" for="{{ $permission->action }}">
+                                    {{ $permission->action }}
+                                </label>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
 
-				<div class="form-group">
-					<button type="submit" class="btn btn-primary mt-3">Update</button>
-				</div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary mt-3">Update</button>
+                </div>
 			</form>
 		</div>
 	</div>
