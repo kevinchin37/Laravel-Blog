10e65274d1fcfce27f5e1e05520bf0c58bf64c63
kevinchin37 <kevin.chin@cbs.com>
2020-05-11 15:35:54 -0400

    Fix spacing/tab issue

--- resources/views/admin/role/create.blade.php
+++ resources/views/admin/role/create.blade.php
@@ -3,33 +3,33 @@
 @section('header_title', 'Create')
 
 @section('main_content')
-	<div class="row post-edit-wrapper">
-		<div class="col-md-12">
-			<form action="/admin/roles" method="POST">
-				@csrf
-				<div class="form-group">
-					<label for="role-name">Name</label>
-					<input id="role-name" class="form-control w-25" type="text" name="name" placeholder="Editor" />
-				</div>
+    <div class="row post-edit-wrapper">
+        <div class="col-md-12">
+            <form action="/admin/roles" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="role-name">Name</label>
+                    <input id="role-name" class="form-control w-25" type="text" name="name" placeholder="Editor" />
+                </div>
 
-				<label>Permissions</label>
-				<div class="form-group">
-					@foreach ($permissions as $permission)
-						<div class="form-check form-check-inline">
-							<div class="form-check-input">
-								<input type="checkbox" name="permissions[]" value="{{ $permission->id }}" id="{{ $permission->action }}"/>
-								<label class="form-check-label" for="{{ $permission->action }}">
-									{{ $permission->action }}
-								</label>
-							</div>
-						</div>
-					@endforeach
-				</div>
+                <label>Permissions</label>
+                <div class="form-group">
+                    @foreach ($permissions as $permission)
+                        <div class="form-check form-check-inline">
+                            <div class="form-check-input">
+                                <input type="checkbox" name="permissions[]" value="{{ $permission->id }}" id="{{ $permission->action }}"/>
+                                <label class="form-check-label" for="{{ $permission->action }}">
+                                {{ $permission->action }}
+                                </label>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
 
-				<div class="form-group">
-					<button type="submit" class="btn btn-primary mt-3">Create</button>
-				</div>
-			</form>
-		</div>
-	</div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary mt-3">Create</button>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
