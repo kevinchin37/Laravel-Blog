250a6e98b4c83f87838edbf6bccdac8318693b6c
kevinchin37 <kevin.chin@cbs.com>
2020-09-18 16:31:49 -0400

    Add a wrapper for tinymce Editor component

---
+++ resources/js/components/Tinymce.vue
@@ -0,0 +1,45 @@
+<template>
+   <div>
+        <Editor api-key="key" :init="settings" :name="formName" :value="formValue"></Editor>
+   </div>
+</template>
+
+<script>
+import Editor from '@tinymce/tinymce-vue';
+
+export default {
+    components: {
+        Editor
+    },
+    props: {
+        name: {
+            default: 'body',
+            type: String
+        },
+        value: {
+            default: ''
+        }
+    },
+    data() {
+        return {
+            settings: {
+                height: 300,
+                menubar: false,
+                toolbar:
+                'undo redo | formatselect | bold italic backcolor | \
+                alignleft aligncenter alignright alignjustify | \
+                bullist numlist outdent indent | removeformat | help'
+            },
+            formName: this.name,
+            formValue: this.value,
+            key: process.env.MIX_TINY_MCE_KEY
+        }
+    },
+    created() {
+        console.log(this.formName);
+    }
+}
+</script>
+
+<style scoped>
+</style>
