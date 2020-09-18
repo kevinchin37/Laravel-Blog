977dec149114784cba870f2667fdf1a6f5908c3b
kevinchin37 <kevin.chin@cbs.com>
2020-09-18 16:41:45 -0400

    Add comments

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,8 +3,60 @@
 @section('pagetitle', $post->title)
 
 @section('content')
-    @component('components.posts.details', ['post' => $post])
-    @endcomponent
+    <div id="post-detail" class="row meta-wrapper">
+        <h1 class="col-md-12">{{ $post->title }}</h1>
+
+        <ul class="col-md-6 meta-list">
+            <li class="post-meta">
+                <span>By:</span> {{ $post->author->name }}
+            </li>
+            <li class="post-meta">
+                <span>Published:</span> {{ $post->created_at }}
+            </li>
+        </ul>
+
+        @if ($post->categories->isNotEmpty() || $post->tags->isNotEmpty())
+            <ul class="col-md-6 meta-list">
+                @if ($post->categories->isNotEmpty())
+                    <li class="post-meta">
+                        <span>Categories:</span>
+                        @foreach ($post->categories as $category)
+                            <a class="taxonomy-link" href="/category/{{ $category->slug }}">{{ $category->name }}</a>
+                        @endforeach
+                    </li>
+                @endif
+
+                @if ($post->tags->isNotEmpty())
+                    <li class="post-meta">
+                        <span>Tags:</span>
+                        @foreach ($post->tags as $tag)
+                            <a class="taxonomy-link" href="/tag/{{ $tag->slug }}">{{ $tag->name }}</a>
+                        @endforeach
+                    </li>
+                @endif
+            </ul>
+        @endif
+    </div>
+
+    <div class="body-content">
+        {!! $post->body !!}
+
+        <form action="/post/comment" method="POST">
+            @csrf
+            <div class="form-group">
+                <div style="border:1px solid #29a19c; padding: 15px;">
+                    <label for="comment-body"><h4 style="">Leave a Comment</h4></label>
+                    <textarea id="comment-body" class="form-control mb-2" cols="30" rows="10" name="body"></textarea>
+                    <button type="submit" id="comment-submit" class="btn btn-primary mt-3">Submit</button>
+                </div>
+
+                <input type="hidden" name="user_id" value="{{ auth()->id() }}">
+                <input type="hidden" name="post_id" value="{{ $post->id }}">
+            </div>
+        </form>
+
+        <comments :post_id="{{ $post->id}}"></comments>
+    </div>
 
     @component('components.posts.blocks', [
         'posts' => $post->author->posts->slice(1)->shuffle()->take(3),
