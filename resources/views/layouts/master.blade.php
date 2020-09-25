19ad86b628cb0a61a4e20d097cfa9588d2631783
kevinchin37 <kevin.chin@cbs.com>
2020-09-25 15:36:19 -0400

    Add app id and include app.js

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,28 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>@yield('pagetitle', 'Home')</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet" type="text/css" >
-</head>
-<body>
-    <section>
-        @include('layouts.header')
+    <html lang="en">
+    <head>
+        <title>@yield('pagetitle', 'Home')</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+        <link href="{{ asset('css/main.css') }}" rel="stylesheet" type="text/css" >
+    </head>
+    <body>
+        <div id="app">
+            @include('layouts.header')
 
-        <div class="container">
-            @yield('latest_post')
+            <div class="container">
+                @yield('latest_post')
 
-            <div class="row">
-                <div class="col-md-9">
-                    @yield('more_posts')
-                </div>
+                <div class="row">
+                    <div class="col-md-9">
+                        @yield('more_posts')
+                    </div>
 
-                <div class="col-md-3">
-                    @include('layouts.sidebar')
+                    <div class="col-md-3">
+                        @include('layouts.sidebar')
+                    </div>
                 </div>
             </div>
         </div>
-    </section>
-    @include('layouts.footer')
-</body>
+        @include('layouts.footer')
+        <script src="/js/app.js"></script>
+    </body>
 </html>
