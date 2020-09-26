40b36a7dd7ac93fa14298a4428507fbdfccb7b21
kevinchin37 <kevin.chin@cbs.com>
2020-09-26 12:24:25 -0400

    Add middleware for admin backend access

---
+++ app/Http/Middleware/CheckAdminAccess.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdminAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user()->hasRole('admin') || Auth::user()->hasRole('editor')) return $next($request);
+        abort(403);
+    }
+}
