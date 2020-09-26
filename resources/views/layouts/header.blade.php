11ec3902b971d53e3e71d374bc1736b350204c09
kevinchin37 <kevin.chin@cbs.com>
2020-09-26 12:25:34 -0400

    Add user menu to header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<nav id="main-menu" class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav id="main-menu" class="navbar navbar-expand-lg navbar-dark">
     <a class="navbar-brand" href="/posts">Site Title</a>
     <div class="collapse navbar-collapse">
         <ul class="navbar-nav">
@@ -13,4 +13,36 @@
             </li>
         </ul>
     </div>
+
+    <div class="user-menu">
+        <div class="collapse navbar-collapse">
+            @if (Auth::check())
+                <div class="avatar-wrapper">
+                    <avatar image="{{ auth()->user()->avatar }}" size="small"></avatar>
+                </div>
+                <span class="welcome-msg">Hi {{ auth()->user()->name }}</span>
+
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Edit Profile</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout">Log Out</a>
+                    </li>
+                </ul>
+            @else
+                <ul class="navbar-nav">
+                    <li>
+                        <a class="nav-link" href="/login">Log In</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="/register">Register</a>
+                    </li>
+                </ul>
+            @endif
+        </div>
+    </div>
+
 </nav>
