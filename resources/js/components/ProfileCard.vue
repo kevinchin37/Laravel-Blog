f531433e50c5c3c56ba9529df2cb5fe77780993f
kevinchin37 <kevin.chin@cbs.com>
2020-09-27 14:34:28 -0400

    Change to use avatar component instead of rendering it's own

--- resources/js/components/ProfileCard.vue
+++ resources/js/components/ProfileCard.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="profile-card">
-        <div class="avatar"
-            :class="{ default: isDefaultAvatar() }"
-            :style="[ isDefaultAvatar() ? '' : { background: 'url(' + this.image.filepath + ') center / cover no-repeat' } ]"
-        ></div>
+        <div class="avatar-wrapper">
+            <avatar :src="this.image" size="large"></avatar>
+        </div>
+
         <div class="control-panel">
             <span>Hi {{ this.name }} </span>
             <div class="options">
@@ -18,7 +18,7 @@
 export default {
     props: {
         'user': Object,
-        'avatar': Object,
+        'avatar': String,
         'preview': {
             type: Boolean,
             default: false
@@ -35,8 +35,7 @@ export default {
     created() {
         if (this.isPreview) {
             this.$eventBus.$on('preview', (newAvatar) => {
-                this.image.filename = 'upload';
-                this.image.filepath = newAvatar;
+                this.image = newAvatar;
             });
         }
     },
@@ -47,51 +46,48 @@ export default {
             }
         },
         isDefaultAvatar() {
-            return (this.image.filename.length == "" || !this.image.filename);
+            return (this.image == "" || !this.image);
         }
     }
 }
 </script>
 
 <style scoped>
-.profile-card {
-    margin: 20px 0;
-    text-align: center;
-}
-.profile-card .avatar {
-    border-radius: 50%;
-    height: 120px;
-    max-width: 120px;
-    margin: 0 auto;
-    margin-bottom: 20px;
-    overflow: hidden;
-    position: relative;
-}
-.profile-card .avatar.default {
-    background: url('/images/placeholders/default-avatar.jpg') center / cover no-repeat;
-}
-.profile-card .control-panel {
-    color: #eeeeee;
-}
-.profile-card .control-panel .options {
-    margin: 0 auto;
-}
-.profile-card .control-panel .options > .option {
-    display: block;
-    margin: 5px 0;
-}
-.profile-card .control-panel .options >.option:hover {
-    text-decoration: none;
-}
-.profile-card .control-panel .options > .option:after {
-    color: #eeeeee;
-    font-family: 'Font Awesome 5 Free';
-    padding-left: 5px;
-}
-.profile-card .control-panel .options > .option.edit:after {
-    content: "\F044";
-}
-.profile-card .control-panel .options > .option.log-out:after {
-    content: "\f2f5";
-}
+    .profile-card {
+        margin: 20px 0;
+        text-align: center;
+    }
+    .profile-card .avatar-wrapper {
+        border-radius: 50%;
+        height: 120px;
+        max-width: 120px;
+        margin: 0 auto;
+        margin-bottom: 20px;
+        overflow: hidden;
+        position: relative;
+    }
+    .profile-card .control-panel {
+        color: #eeeeee;
+    }
+    .profile-card .control-panel .options {
+        margin: 0 auto;
+    }
+    .profile-card .control-panel .options > .option {
+        display: block;
+        margin: 5px 0;
+    }
+    .profile-card .control-panel .options >.option:hover {
+        text-decoration: none;
+    }
+    .profile-card .control-panel .options > .option:after {
+        color: #eeeeee;
+        font-family: 'Font Awesome 5 Free';
+        padding-left: 5px;
+    }
+    .profile-card .control-panel .options > .option.edit:after {
+        content: "\F044";
+    }
+    .profile-card .control-panel .options > .option.log-out:after {
+        content: "\f2f5";
+    }
 </style>
