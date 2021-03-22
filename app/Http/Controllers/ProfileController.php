ea8baf53df036f84c234d352e6b4f7a4d3a68008
kevinchin37 <kevin.chin@cbs.com>
2021-03-22 12:29:23 -0400

    Add update method for public profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
@@ -17,5 +18,40 @@ class ProfileController extends Controller
         return view('profile.edit', [
             'user' => $user
         ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(User $user)
+    {
+        $attributes = request()->validate([
+            'name' => 'required|min:5|max:15',
+            'avatar' => 'nullable|image|max:1500',
+            'email' => 'unique:users,email,' . $user->id,
+            'password' => 'nullable|min:8|confirmed',
+        ]);
+
+        if (!empty($attributes['avatar'])) {
+            Storage::disk('public')->delete($user->avatar);
+            $attributes['avatar'] = resizeAndStoreImage($attributes['avatar'], [
+                'prefix' => 'avatar',
+                'path' => '/uploads/avatars/' . $user->id,
+            ]);
+        }
+
+        if (!empty($attributes['password'])) {
+            $attributes['password'] = Hash::make($attributes['password']);
+        } else {
+            unset($attributes['password']);
+        }
+
+        $user->update($attributes);
+
+        return back()->with('status', 'Profile has been updated.');
     }
 }
