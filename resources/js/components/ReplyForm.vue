4ebbb716bfe8535ce08994654442989851e574d6
kevinchin37 <kevin.chin@cbs.com>
2021-03-22 11:42:48 -0400

    Add thread under reply form

--- resources/js/components/ReplyForm.vue
+++ resources/js/components/ReplyForm.vue
@@ -1,13 +1,16 @@
 <template>
-   <div class="editor">
-        <a href="#reply" class="float-right mb-3" @click="renderForm">Reply</a>
+    <div class="editor">
+        <a href="#reply" class="float-right mb-3" @click="toggleForm">Reply</a>
 
         <form @submit="submitForm" v-if="formActive">
+            <div v-if="!this.isLoggedIn" class="alert alert-danger">Must be logged in to leave a comment.</div>
             <input type="hidden" name="_token" :value="csrf">
             <Editor api-key="my-key" :init="settings" name="body" v-model="bodyText"></Editor>
-            <button type="submit" class="btn btn-primary mt-3">Submit</button>
+            <button type="submit" class="btn btn-primary mt-3" :disabled="!this.isLoggedIn">Submit</button>
         </form>
-   </div>
+
+        <replies :thread="this.parentId"></replies>
+    </div>
 </template>
 
 <script>
@@ -26,10 +29,10 @@ export default {
             type: Boolean,
             default: false
         },
-        parent: {
-            type: Number
+        parentComment: {
+            type: Number,
         },
-        post: {
+        postId: {
             type: Number
         },
     },
@@ -43,27 +46,32 @@ export default {
                 alignleft aligncenter alignright alignjustify | \
                 bullist numlist outdent indent | removeformat | help'
             },
-            formAction: this.action,
             formActive: false,
             csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
             bodyText: '',
-            parentId: this.parent,
-            postId: this.post,
+            parentId: this.parentComment,
+            isLoggedIn: $('meta[name=isLoggedIn]').attr('content'),
         }
     },
     methods: {
-        renderForm(e) {
+        toggleForm(e) {
             e.preventDefault();
+            this.bodyText = '';
             this.formActive = !this.formActive;
         },
 
         submitForm(e) {
-            axios.post(this.formAction, {
+            e.preventDefault();
+
+            axios.post(this.action, {
                 body: this.bodyText,
                 post_id: this.postId,
                 parent_id: this.parentId,
-            }).then(res=>console.log(res))
-            .catch(err=>console.log(err))
+            }).then(res => {
+                this.$eventBus.$emit('newReply', this.parentId);
+                this.toggleForm(e);
+            })
+            .catch(err => console.log(err))
         }
     }
 }
