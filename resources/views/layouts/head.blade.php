ceb6f3fc7518bd5b1e3eba41d24c02e08d09bbea
kevinchin37 <kevin.chin@cbs.com>
2021-03-22 11:52:53 -0400

    Update placeholder thumbnail image

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,4 @@
+<title>@yield('pagetitle', 'Blog')</title>
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+<link href="{{ asset('css/main.css') }}" rel="stylesheet" type="text/css" >
